5d7cf516d4d12a6c5812701f674092e8bc0cc5dc
Christoph Matthias Kohnen <mail@cmkohnen.de>
2026-06-24 21:52:24 +0200

    Implement camera effects
    Signed-off-by: Christoph Matthias Kohnen <mail@cmkohnen.de>

--- src/renderer/postProcessing.c
+++ src/renderer/postProcessing.c
@@ -1,3 +1,6 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include "../../headers/core/window.h"
 #include "../../headers/renderer/mesh.h"
 #include "../../headers/scene/scene.h"
@@ -5,12 +8,16 @@
 void
 render_scene (Scene *scene, Window *window)
 {
+  GLfloat time = glfwGetTime ();
+
   glClear (GL_COLOR_BUFFER_BIT);
 
   glUseProgram (window->screen_shader);
 
   glActiveTexture (GL_TEXTURE0);
   glBindTexture (GL_TEXTURE_2D, scene->texturebuffer);
+
+  glUniform1f (glGetUniformLocation (window->screen_shader, "time"), time);
 
   glBindVertexArray (window->vertex_array);
   glDrawArrays (GL_TRIANGLES, 0, 6);
