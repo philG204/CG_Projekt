385ad3630be9b87121e30a7d235d936423be9779
Bennet Kreth <114832850+Spacebrezel@users.noreply.github.com>
2026-06-25 00:35:38 +0200

    texture mapping works mostly

--- assets/vertex.glsl
+++ assets/vertex.glsl
@@ -7,6 +7,8 @@ out vec2 TexCoord;
 
 void main()
 {
-    TexCoord = aTexCoord;
+    //TexCoord = aTexCoord;
+    TexCoord = 1.0- vec2(1.0 - aTexCoord.x, 1.0 - aTexCoord.y);
+    
     gl_Position = vec4(aPos, 0.0, 1.0);
 }
