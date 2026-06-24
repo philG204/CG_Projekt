08104a38df8fbf2eb5f1d6c9aca3720124acd0fc
Christoph Matthias Kohnen <mail@cmkohnen.de>
2026-06-24 11:49:31 +0200

    add post-processing pipeline
    Signed-off-by: Christoph Matthias Kohnen <mail@cmkohnen.de>

--- assets/fragment.glsl
+++ assets/fragment.glsl
@@ -8,8 +8,5 @@ out vec4 FragColor;
 
 void main()
 {
-    vec4 color = texture(screenTexture, TexCoord);
-
-    // invert sample
-    FragColor = vec4(1.0 - color.rgb, color.a);
+    FragColor = texture(screenTexture, TexCoord);
 }
